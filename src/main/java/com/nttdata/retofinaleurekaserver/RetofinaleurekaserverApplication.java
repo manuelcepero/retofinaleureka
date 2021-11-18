d687287109ecdf6e25d0dc55de8bbeb9083febbe
Manuel Cepero Sánchez <manuel.cepero@hotmail.com>
2021-11-18 15:01:33 +0100

    Subida del proyecto

---
+++ src/main/java/com/nttdata/retofinaleurekaserver/RetofinaleurekaserverApplication.java
@@ -0,0 +1,15 @@
+package com.nttdata.retofinaleurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RetofinaleurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetofinaleurekaserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/retofinaleurekaserver/RetofinaleurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.retofinaleurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetofinaleurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
